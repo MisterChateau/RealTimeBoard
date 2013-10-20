7a680ac36bc2bbdd61ebe38788d79d9ad7bfce03
MisterChateau <jchateau@gmail.com>
2013-10-20 22:55:55 +0200

    bug fix`

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,36 @@
-var board = {
+$(window).ready(function () {
 
-	init: function(){
-		var serverBaseUrl = document.domain;
-		var socket = io.connect(serverBaseUrl);
+    //use module pattern
+    var board = {
+        init: function () {
+            var serverBaseUrl = document.domain;
+            var socket = io.connect(serverBaseUrl);
 
-		$("button").on("click", this.addTopic);
+            $("button").on("click", this.addTopic);
+            //bind to the socket.io "newTopic" event
+            socket.on("newTopic", function (data) {
+                    var html = "<li class='col-sm-12'>" + data + "</li>";
+                    $("#toDiscuss").append(html);
+            });
+        },
 
-		socket.on("newTopic", function(data){
-		var html = "<li class='col-sm-12'>"+data+"</li>";
-		console.log(html);
-		$("#toDiscuss").append(html);
-		});
-	},
+        addTopic: function () {
+            //send Post to express API
+            var value = $("input").val();
+            $.ajax({
+                type: "POST",
+                url: "/topic",
+                dataType: "JSON",
+                data: {topic: value}
+            })
+                .done(function (msg) {
+                    console.log("Success " + msg);
+                })
+                .fail(function (msg) {
+                    console.log("Fail " + msg);
+                })
+        }
+}
 
-	addTopic: function(){
-
-		var value = $("input").val();
-
-		$.ajax({
-			type: "POST",
-			url: "/topic",
-			dataType: "JSON",
-			data: {topic: value}
-		})
-		.done(function(msg){
-			console.log("Success");
-		})
-		.fail(function(textstatus){
-			console.log("Fail");
-		})
-	}
-};
-
-board.init();
+    board.init();
+});
--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-/**
-* Module requirements.
-*/
 
+//import Modules
 var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io").listen(http);
 var path = require("path");
 
-/**
-* Configuration
-*/
+//Configure express app
+//Set static file path
 app.use(express.static(path.join(__dirname, 'public')));
+//add JSON support
 app.use(express.bodyParser());
+//set port
 app.set("port", 80);
+//set view engine
 app.set("view engine", "jade");
+//set view directory
 app.set("views", __dirname + "/views");
-app.set("view options", { layout: false });
 
-/**
-* Configuration
-*/
+//configure routes
 app.get("/", function(req, res){
 	res.render("index");
 	}
@@ -31,4 +29,5 @@ app.post("/topic", function(req, res) {
 	io.sockets.emit("newTopic", msg);
 });
 
+//start the server
 http.listen(app.get("port"));
